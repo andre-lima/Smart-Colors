5c38e02d8c1f7f348080b20743f4f216371df8bf
Andre Dantas <andre.dantas.lima@gmail.com>
2017-07-02 20:54:07 -0400

    Adding chalk to handle colors

--- src/printPalette.js
+++ src/printPalette.js
@@ -1,26 +1,31 @@
-import supportsColor from 'supports-color';
-import { terminal as term } from 'terminal-kit';
+import chalk from 'chalk';
 
 function printPalette(colors, url) {
   // Won't display palette if terminal doesn't support colors
-  if (!supportsColor) {
+  if (chalk.level === 0) {
     return;
   }
+  
+  console.log(chalk`{underline                                                                                                           }`);
+  console.log(chalk`{blueBright                                                                                            >> SMART COLORS}`);
 
-  // Will display warning if terminal doesn't support 24bit colors
-  const colorSupportWarning = supportsColor.has16m ? '' : 'Note: This palette is an approximation of the true colors displayed on you browser\n';
-
-  term.dim(colorSupportWarning);
-
+  let colorNames = '';
+  let palette = '';
   colors.forEach((color) => {
-    term.bgColorRgbHex(color);
-    term.brightBlack.bold(`   ${color}   `);
+    colorNames += `      ${color}      `;
+    palette += chalk`{bgHex(${color})                    }`;
   });
+  palette += chalk`{reset }`;  //Resets colors to default values
 
-  term('\n');
-  term.bgDefaultColor();
+  console.log(colorNames);
+  console.log(palette);
+  console.log(`Link to this palette: ${url}`);
+  
+  // Will display warning if terminal doesn't support 24bit colors
+  if(chalk.level <= 2)
+    console.log(chalk.dim('\nNote: This palette is an approximation of the true colors displayed on you browser'));
 
-  term(`Link to this palette: ${url} \n\n`);
+  console.log(chalk`{underline                                                                                                           }`);
 }
 
 module.exports = printPalette;
